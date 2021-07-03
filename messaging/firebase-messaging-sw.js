dac43dd5f243359ecbaa4a278aaef2badd509d0a
frankie@timebird.org <frank@gmail.com>
2021-07-03 11:02:36 +0700

    Add init

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,9 +1,26 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/8.7.0/firebase-app.js');
-importScripts('/__/firebase/8.7.0/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.7.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.7.0/firebase-messaging.js');
+// importScripts('./init.js');
+
+importScripts('https://www.gstatic.com/firebasejs/8.7.0/firebase-app.js');
+ importScripts('https://www.gstatic.com/firebasejs/8.7.0/firebase-messaging.js');
+
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBNAXWNjJ6fpkNaL7tmeWu9DfohWMPV5fw",
+  authDomain: "geswallet-messaging.firebaseapp.com",
+  databaseURL: "https://geswallet-messaging.firebaseio.com",
+  projectId: "geswallet-messaging",
+  storageBucket: "geswallet-messaging.appspot.com",
+  messagingSenderId: "784571921152",
+  appId: '1:784571921152:web:7dfd8b42aca447592f79e5'
+};
+firebase.initializeApp(config);
 
 const messaging = firebase.messaging();
 
---
+++ messaging/init.js
@@ -0,0 +1,11 @@
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyBNAXWNjJ6fpkNaL7tmeWu9DfohWMPV5fw",
+  authDomain: "geswallet-messaging.firebaseapp.com",
+  databaseURL: "https://geswallet-messaging.firebaseio.com",
+  projectId: "geswallet-messaging",
+  storageBucket: "geswallet-messaging.appspot.com",
+  messagingSenderId: "784571921152",
+  appId: '1:784571921152:web:7dfd8b42aca447592f79e5'
+};
+firebase.initializeApp(config);
